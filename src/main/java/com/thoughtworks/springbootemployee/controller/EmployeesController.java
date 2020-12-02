a8809a8fa14a904fe7b8139723a6d14d89058e4d
kenckl <klcheung8355@gmail.com>
2020-12-02 19:12:17 +0800

    [Spring Boot Employee][Ken] Implemented createEmployee function

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -3,9 +3,7 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.Model.Employee;
 import com.thoughtworks.springbootemployee.Repository.EmployeeRepository;
 import com.thoughtworks.springbootemployee.Service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,5 +17,11 @@ public class EmployeesController {
     @GetMapping
     public List<Employee> getEmployeesList(){
         return employees;
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee createEmployee){
+        employees.add(createEmployee);
+        return createEmployee;
     }
 }
