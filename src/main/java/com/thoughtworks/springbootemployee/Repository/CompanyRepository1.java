2f604b57c4c4c262548e25a34bf6a714d4792cd2
kenckl <klcheung8355@gmail.com>
2020-12-06 04:07:41 +0800

    [Spring Boot Company][Ken] Code refactor

--- src/main/java/com/thoughtworks/springbootemployee/Repository/CompanyRepository1.java
+++ src/main/java/com/thoughtworks/springbootemployee/Repository/CompanyRepository1.java
@@ -4,8 +4,6 @@ import com.thoughtworks.springbootemployee.Model.Company;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CompanyRepository1 extends MongoRepository<Company,String>{
     Company findByCompanyId(String id);
